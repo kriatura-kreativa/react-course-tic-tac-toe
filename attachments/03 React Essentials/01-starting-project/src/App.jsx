84e18fc99e32eef5235024e9d122bba5f6fb4da8
kriatura-kreativa <137964177+kriatura-kreativa@users.noreply.github.com>
2024-02-25 11:17:20 -0400

    Added click handlers to TabButtons

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,8 +1,14 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log(`hello World! - selected`)
+  }
+
+
   return (
     <div>
       <Header />
@@ -15,6 +21,18 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+
+          Dynamic Content
+
         </section>
       </main>
     </div>
