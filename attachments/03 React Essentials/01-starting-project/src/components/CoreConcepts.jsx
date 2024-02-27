8fa269ff896e1a4fffcd4f1107cb91bdea656a97
kriatura-kreativa <137964177+kriatura-kreativa@users.noreply.github.com>
2024-02-26 23:43:57 -0400

    Created components, cleaned up Codebase

---
+++ attachments/03 React Essentials/01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,17 @@
+import { CORE_CONCEPTS } from "../data-with-examples.js";
+import CoreConcept from "../components/CoreConcept.jsx";
+
+export default function CoreConcepts() {
+  return (
+    <>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          {CORE_CONCEPTS.map((conceptItem) => (
+            <CoreConcept key={conceptItem.title} {...conceptItem} />
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+}
