59c8080b04feea962a7ca452eba9a4c4ecd872c5
kriatura-kreativa <137964177+kriatura-kreativa@users.noreply.github.com>
2024-02-29 19:28:48 -0400

    Updated GameBoard

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,5 +1,7 @@
 // array.map(function(currentValue, index, arr), thisValue)
 
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -7,14 +9,28 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameboard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameboard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
